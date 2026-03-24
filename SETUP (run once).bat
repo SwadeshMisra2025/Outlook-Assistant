4f96aa84c9ed77b5460116995385c62edde9c281
SwadeshMisra2025 <swadesh.misra@aletha.ca>
2026-03-24 10:26:38 -0400

    feat: add double-click .bat launchers for non-developer users

---
+++ SETUP (run once).bat
@@ -0,0 +1,5 @@
+@echo off
+:: Double-click this file once to install everything.
+:: You only need to run this ONE TIME on a new machine.
+powershell.exe -ExecutionPolicy Bypass -File "%~dp0setup.ps1"
+pause
---
+++ START Outlook Assistant.bat
@@ -0,0 +1,4 @@
+@echo off
+:: Double-click this file every time you want to use Outlook Assistant.
+powershell.exe -ExecutionPolicy Bypass -File "%~dp0start.ps1"
+pause
